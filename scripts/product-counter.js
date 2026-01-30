512789608c94872660873d22ccf483c80dec4aa8
Arjan <aup.timalsina@gmail.com>
2026-01-30 10:10:16 +0545

    Individual pages and comments

---
+++ scripts/product-counter.js
@@ -0,0 +1,16 @@
+// Increment the product counter
+function incrementCounter(btn) {
+  const valueElem = btn.parentElement.querySelector('.counter-value');
+  let value = parseInt(valueElem.textContent, 10);
+  valueElem.textContent = value + 1;
+}
+
+// Decrement the product counter, minimum value is 1
+function decrementCounter(btn) {
+  const valueElem = btn.parentElement.querySelector('.counter-value');
+  let value = parseInt(valueElem.textContent, 10);
+  if (value > 1) {
+    valueElem.textContent = value - 1;
+  }
+}
+
