dfabc6a9f61697fb86c281fceb34f2b97ab8e8c0
SnipedSkull <88181462+SnipedSkull@users.noreply.github.com>
2026-01-30 12:00:57 +0545

    products

--- scripts/product-counter.js
+++ scripts/product-counter.js
@@ -2,7 +2,9 @@
 function incrementCounter(btn) {
   const valueElem = btn.parentElement.querySelector('.counter-value');
   let value = parseInt(valueElem.textContent, 10);
+  if (value < 5) {
   valueElem.textContent = value + 1;
+  }
 }
 
 // Decrement the product counter, minimum value is 1
